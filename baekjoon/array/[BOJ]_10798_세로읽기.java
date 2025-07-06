59a780d132636add36b081e71867dd4682df4d26
bbangjae <thebyunjae@naver.com>
2025-07-06 20:55:28 +0900

    review(array): [BOJ]_10798_세로읽기 기존 int 배열 및 -1 마킹 대신 char 배열과 '\0' 체크로 단순화

--- baekjoon/array/[BOJ]_10798_세로읽기.java
+++ baekjoon/array/[BOJ]_10798_세로읽기.java
@@ -6,7 +6,7 @@ public class [BOJ]_10798_세로읽기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
-        int[][] arr = new int[5][15];
+        char[][] arr = new char[5][15];
 
         for (int i = 0; i < 5; i++) {
             String line = br.readLine();
@@ -14,21 +14,14 @@ public class [BOJ]_10798_세로읽기 {
             for (int j = 0; j < line.length(); j++) {
                 arr[i][j] = line.charAt(j);
             }
-            if (line.length() != 15) {
-                arr[i][line.length()] = -1;
-            }
         }
         br.close();
 
         for (int i = 0; i < 15 ; i++) {
             for (int j = 0; j < 5 ; j++) {
-                if (arr[j][i] == -1) {
-                    if (i == 14)
-                        continue;
-                    arr[j][i + 1] = -1;
-                    continue;
+                if (arr[j][i] != '\0') {
+                    bw.write(arr[j][i]);
                 }
-                bw.write(arr[j][i]);
             }
         }
         bw.flush();
